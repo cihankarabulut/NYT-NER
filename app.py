51b9ece82b086cbde730f8fff70f3ce847f65615
cihankarabulut <cihan_karabulut@yahoo.com>
2022-04-23 14:45:56 -0400

    added try-except block to check if the model is present

--- app.py
+++ app.py
@@ -7,12 +7,24 @@ Created on Fri Apr 22 20:58:58 2022
 
 import spacy
 import spacy_streamlit
-import streamlit as st
 
-models = ["en_core_web_md"]
-default_text = ""
 
-spacy_streamlit.visualize(models, default_text, visualizers=["ner"],
-                          show_pipeline_info=False, show_config=False,
-                          show_json_doc=False, show_meta=False,
-                          ner_attrs = ["text", "label_"])
+def app():
+        
+    try:
+        models = ["en_core_web_md"]
+    except: # If not present, we download
+        spacy.cli.download("en_core_web_md")
+        models = ["en_core_web_md"]    
+        
+    
+    default_text = ""
+        
+    spacy_streamlit.visualize(models, default_text, visualizers=["ner"],
+                              show_pipeline_info=False, show_config=False,
+                              show_json_doc=False, show_meta=False,
+                              ner_attrs = ["text", "label_"])
+
+
+if __name__ == '__main__':
+    app()
